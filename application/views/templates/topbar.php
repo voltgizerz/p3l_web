a88e82165b8cea422989ef56b85dbb29bff8ce4c
voltgizerz <felix.asui@gmail.com>
2020-04-29 13:07:56 +0700

    fix blocked message

--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -18,14 +18,14 @@
             <ul class="navbar-nav ml-auto">
 
 
-
                 <div class="topbar-divider d-none d-sm-block"></div>
 
                 <!-- Nav Item - User Information -->
                 <li class="nav-item dropdown no-arrow">
                     <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
                         aria-haspopup="true" aria-expanded="false">
-                        <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $user['nama_pegawai']; ?></span>
+                        <span class="mr-2 d-none d-lg-inline text-gray-600 small">Hello,
+                            <?= $user['nama_pegawai']; ?></span>
 
                     </a>
                     <!-- Dropdown - User Information -->
