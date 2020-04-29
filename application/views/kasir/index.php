ad7c389a9471c8920760c5156043d5be0aab1248
voltgizerz <felix.asui@gmail.com>
2020-04-29 14:39:36 +0700

    berhasil buat dashboard kasir

--- application/views/kasir/index.php
+++ application/views/kasir/index.php
@@ -2,29 +2,12 @@
 <div class="container-fluid">
 
     <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
-    <div class="row">
-        <div class="col-lg-8">
-            <?= $this->session->flashdata('message'); ?>
-        </div>
-        <div class="card mb-3" style="max-width: 540px;">
-            <div class="row no-gutters">
-                <div class="col-md-4-mt-3">
-                    <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="card-img">
-                </div>
-                <div class="col">
-                    <div class="card-body">
-                        <h5 class="card-title"><?= $user['name']; ?></h5>
-                        <p class="card-text"><?= $user['email']; ?></p>
-                        <p class="card-text"><small class="text-muted">Member Since :
-                                <?= date('d F Y', $user['datecreated']); ?></small></p>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <h1 class="h3 mb-4 text-gray-800"><?= $title ?> - Kasir AREA</h1>
+    <h2 class="h3 mb-4 text-gray-800">Selamat Datang Kembali Kasir</h2>
+    <img src=" <?= base_url('assets/img/petshop2.jpg'); ?>" width="100%" height="400">
 
-    </div>
-    <!-- /.container-fluid -->
+</div>
+<!-- /.container-fluid -->
 
 </div>
 <!-- End of Main Content -->
