8e1528d1cf583f786d5e8aea1131fde9272e526e
Felix Fernando <54938467+voltgizerz@users.noreply.github.com>
2020-05-24 12:49:05 +0700

    nota lunas pembayaran produk done

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -2,14 +2,14 @@
 class Laporan extends CI_Controller
 {
 
-    function __construct()
+    public function __construct()
     {
         parent::__construct();
         $this->load->library('pdf');
         date_default_timezone_set("Asia/Bangkok");
     }
 
-    function index($id)
+    public function index($id)
     {
         $kode = $this->db->get_where('data_pengadaan', ['id_pengadaan' => $id])->row()->kode_pengadaan;
         $tanggal = $this->db->get_where('data_pengadaan', ['id_pengadaan' => $id])->row()->created_date;
@@ -17,17 +17,17 @@ class Laporan extends CI_Controller
         $namaSupplier = $this->db->get_where('data_supplier', ['id_supplier' => $id_supplier])->row()->nama_supplier;
         $alamatSupplier = $this->db->get_where('data_supplier', ['id_supplier' => $id_supplier])->row()->alamat_supplier;
         $telp = $this->db->get_where('data_supplier', ['id_supplier' => $id_supplier])->row()->nomor_telepon_supplier;
-       
-        $new_date = date('d F Y',strtotime($tanggal));
+
+        $new_date = date('d F Y', strtotime($tanggal));
         $cnt = 1;
-        $pdf = new FPDF('P','mm',array(210,210));
+        $pdf = new FPDF('P', 'mm', array(210, 210));
         // membuat halaman baru
         $pdf->AddPage();
         // setting jenis font yang akan digunakan
         //HEADER LAPORAN
         $pdf->SetFont('Arial', 'B', 16);
         $pdf->Rect(5, 5, 200, 200, 'D');
-        $pdf->Image('D:\XAMPP\htdocs\p3l_web\assets\img\headerlaporan.png',7,10,195,0,'PNG');
+        $pdf->Image('D:\XAMPP\htdocs\p3l_web\assets\img\headerlaporan.png', 7, 10, 195, 0, 'PNG');
 
         //TEXT
         $pdf->Cell(10, 60, '', 0, 1);
@@ -35,12 +35,12 @@ class Laporan extends CI_Controller
         $pdf->Cell(10, 7, '', 0, 1);
         // KODE PENGADDAN
         $pdf->SetFont('Arial', 'B', 10);
-        $pdf->Cell(300, 0, 'No : '.$kode, 99, 1, 'C');
+        $pdf->Cell(300, 0, 'No : ' . $kode, 99, 1, 'C');
         $pdf->Cell(10, 7, '', 0, 1);
         //TANGGAL PENGADAAN
         $pdf->SetFont('Arial', 'B', 10);
-        $pdf->Cell(300, 0, 'Tanggal : '.$new_date, 99, 1, 'C');
-        $pdf->Cell(1,1, '', 0, 1);
+        $pdf->Cell(300, 0, 'Tanggal : ' . $new_date, 99, 1, 'C');
+        $pdf->Cell(1, 1, '', 0, 1);
         //TANGGAL PENGADAAN
         $pdf->SetLeftMargin(28);
         $pdf->SetFont('Arial', '', 10);
@@ -67,8 +67,8 @@ class Laporan extends CI_Controller
         $pdf->Cell(65, 5, 'Nama Produk', 1, 0, 'C');
         $pdf->Cell(35, 5, 'Satuan', 1, 0, 'C');
         $pdf->Cell(40, 5, 'Jumlah', 1, 1, 'C');
-        $pdf->SetFillColor(193,229,252);
-        
+        $pdf->SetFillColor(193, 229, 252);
+
         $this->db->select('data_detail_pengadaan.id_detail_pengadaan,data_detail_pengadaan.id_produk_fk,data_produk.nama_produk,data_produk.gambar_produk,data_detail_pengadaan.kode_pengadaan_fk,data_detail_pengadaan.satuan_pengadaan,data_detail_pengadaan.jumlah_pengadaan,data_detail_pengadaan.tanggal_pengadaan');
         $this->db->join('data_produk', 'data_produk.id_produk = data_detail_pengadaan.id_produk_fk');
         $this->db->from('data_detail_pengadaan');
@@ -86,25 +86,40 @@ class Laporan extends CI_Controller
         }
         $pdf->Cell(10, 20, '', 0, 1);
         $pdf->SetFont('Arial', '', 10);
-        $pdf->Cell(279, 0, 'Dicetak Tanggal '.date('d F Y'), 99, 1, 'C');
-        $pdf->Output("I","[PENGADAAN] Struk - ".$kode.".pdf");
+        $pdf->Cell(279, 0, 'Dicetak Tanggal ' . date('d F Y'), 99, 1, 'C');
+        $pdf->Output("I", "[PENGADAAN] Struk - " . $kode . ".pdf");
     }
 
-    function strukLunasProduk($id)
+    public function strukLunasProduk($id)
     {
         $kode = $this->db->get_where('data_transaksi_penjualan_produk', ['id_transaksi_penjualan_produk' => $id])->row()->kode_transaksi_penjualan_produk;
         $subtotal = $this->db->get_where('data_transaksi_penjualan_produk', ['id_transaksi_penjualan_produk' => $id])->row()->total_penjualan_produk;
         $diskon = $this->db->get_where('data_transaksi_penjualan_produk', ['id_transaksi_penjualan_produk' => $id])->row()->diskon;
         $total = $this->db->get_where('data_transaksi_penjualan_produk', ['id_transaksi_penjualan_produk' => $id])->row()->total_harga;
+        $idHewan = $this->db->get_where('data_transaksi_penjualan_produk', ['id_transaksi_penjualan_produk' => $id])->row()->id_hewan;
+        $idCs = $this->db->get_where('data_transaksi_penjualan_produk', ['id_transaksi_penjualan_produk' => $id])->row()->id_cs;
+        $idKasir = $this->db->get_where('data_transaksi_penjualan_produk', ['id_transaksi_penjualan_produk' => $id])->row()->id_kasir;
+        $namaCs =$this->db->get_where('data_pegawai', ['id_pegawai' => $idCs])->row()->nama_pegawai;
+        $namaKasir =$this->db->get_where('data_pegawai', ['id_pegawai' => $idKasir])->row()->nama_pegawai;
+        
+        if ($idHewan != 0) {
+            $namaHewan = $this->db->get_where('data_hewan', ['id_hewan' => $idHewan])->row()->nama_hewan;
+            $idJenis = $this->db->get_where('data_hewan', ['id_hewan' => $idHewan])->row()->id_jenis_hewan;
+            $nama_jenis_hewan = $this->db->get_where('data_jenis_hewan', ['id_jenis_hewan' => $idJenis])->row()->nama_jenis_hewan;
+            $idCutomer = $this->db->get_where('data_hewan', ['id_hewan' => $idHewan])->row()->id_customer;
+            $nama_customer = $this->db->get_where('data_customer', ['id_customer' => $idCutomer])->row()->nama_customer;
+            $telepon = $this->db->get_where('data_customer', ['id_customer' => $idCutomer])->row()->nomor_hp_customer;
+        
+        }
         $cnt = 1;
-        $pdf = new FPDF('P','mm',array(210,210));
+        $pdf = new FPDF('P', 'mm', array(210, 210));
         // membuat halaman baru
         $pdf->AddPage();
         // setting jenis font yang akan digunakan
         //HEADER LAPORAN
         $pdf->SetFont('Arial', 'B', 16);
         $pdf->Rect(5, 5, 200, 200, 'D');
-        $pdf->Image('D:\XAMPP\htdocs\p3l_web\assets\img\headerlaporan.png',7,10,195,0,'PNG');
+        $pdf->Image('D:\XAMPP\htdocs\p3l_web\assets\img\headerlaporan.png', 7, 10, 195, 0, 'PNG');
 
         //TEXT
         $pdf->Cell(10, 60, '', 0, 1);
@@ -120,22 +135,30 @@ class Laporan extends CI_Controller
         $pdf->Cell(100, 0, $kode, 99, 1, 'L');
         $pdf->Cell(10, 10, '', 0, 1);
         $pdf->SetFont('Arial', '', 10);
-        $pdf->Cell(80, 0,'Member : ', 99, 1, 'L');
-        $pdf->Cell(150, 0,'CS    : Felix Fernando Wijaya ', 99, 1, 'R');
+        if ($idHewan != 0) {
+            $pdf->Cell(80, 0, 'Member : ' . $nama_customer . ' (' . $namaHewan . ' - ' . $nama_jenis_hewan . ')', 99, 1, 'L');
+        } else {
+            $pdf->Cell(80, 0, 'Non Member ', 99, 1, 'L');
+        }
+        $pdf->Cell(150, 0, 'CS    : '.$namaCs, 99, 1, 'R');
         $pdf->Cell(10, 7, '', 0, 1);
         $pdf->SetFont('Arial', '', 10);
-        $pdf->Cell(80, 0,'Telepon : ', 99, 1, 'L');
-        $pdf->Cell(135, 0,'Kasir : Bang wawan ', 99, 1, 'R');
+        if ($idHewan != 0) {
+            $pdf->Cell(80, 0, 'Telepon : '.$telepon, 99, 1, 'L');
+        } else {
+            $pdf->Cell(80, 0, 'Telepon : - ', 99, 1, 'L');
+        }
+        $pdf->Cell(150, 0, 'Kasir : '.$namaKasir, 99, 1, 'R');
         $pdf->Cell(10, 7, '', 0, 1);
         $pdf->SetFont('Arial', '', 10);
         $pdf->Cell(10, 7, '', 0, 1);
         $pdf->SetFont('Arial', '', 10);
-        $pdf->Image('D:\XAMPP\htdocs\p3l_web\assets\img\garis.png',23,110,160,0,'PNG');
+        $pdf->Image('D:\XAMPP\htdocs\p3l_web\assets\img\garis.png', 23, 110, 160, 0, 'PNG');
         $pdf->SetFont('Arial', 'B', 16);
         $pdf->Cell(154, 1, 'Produk', 4, 1, 'C');
         $pdf->Cell(10, 7, '', 0, 1);
         $pdf->SetFont('Arial', '', 10);
-        $pdf->Image('D:\XAMPP\htdocs\p3l_web\assets\img\garis.png',23,125,160,0,'PNG');
+        $pdf->Image('D:\XAMPP\htdocs\p3l_web\assets\img\garis.png', 23, 125, 160, 0, 'PNG');
         // mencetak string
         // Memberikan space kebawah agar tidak terlalu rapat
         $pdf->SetLeftMargin(18);
@@ -146,7 +169,7 @@ class Laporan extends CI_Controller
         $pdf->Cell(35, 5, 'Harga', 1, 0, 'C');
         $pdf->Cell(20, 5, 'Jumlah', 1, 0, 'C');
         $pdf->Cell(40, 5, 'Subtotal Per Item', 1, 1, 'C');
-        
+
         $this->db->select('data_detail_penjualan_produk.id_detail_penjualan_produk,data_detail_penjualan_produk.kode_transaksi_penjualan_produk_fk,data_detail_penjualan_produk.id_produk_penjualan_fk,data_detail_penjualan_produk.jumlah_produk,data_detail_penjualan_produk.subtotal,data_produk.nama_produk,data_produk.gambar_produk,data_produk.harga_produk');
         $this->db->join('data_produk', 'data_produk.id_produk = data_detail_penjualan_produk.id_produk_penjualan_fk');
         $this->db->from('data_detail_penjualan_produk');
@@ -157,30 +180,26 @@ class Laporan extends CI_Controller
         foreach ($produk as $row) {
             $pdf->SetFont('Arial', '', 10);
             $pdf->Cell(10, 5, $cnt, 1, 0, 'C', 0);
-            $pdf->Cell(65, 5,$row->nama_produk, 1, 0);
-            $pdf->Cell(35, 5,'Rp.   '.$row->harga_produk.',-', 1, 0);
+            $pdf->Cell(65, 5, $row->nama_produk, 1, 0);
+            $pdf->Cell(35, 5, 'Rp.   ' . $row->harga_produk . ',-', 1, 0);
             $pdf->Cell(20, 5, $row->jumlah_produk, 1, 0, 'C');
-            $pdf->Cell(40, 5,'Rp.   '.$row->harga_produk*$row->jumlah_produk.',-', 1, 1);
+            $pdf->Cell(40, 5, 'Rp.   ' . $row->harga_produk * $row->jumlah_produk . ',-', 1, 1);
             $cnt++;
         }
         $pdf->SetLeftMargin(134);
-        $pdf->Cell(10, 5, '', 0, 1);
-        $pdf->SetFont('Arial', '', 10);
-        $pdf->Cell(154, 0, 'Subtotal Rp.   '.$subtotal.',-', 99, 1, 'L');
         $pdf->Cell(10, 7, '', 0, 1);
         $pdf->SetFont('Arial', '', 10);
-        $pdf->Cell(154, 0, 'Diskon   Rp.   '.$diskon.',-', 99, 1, 'L');
+        $pdf->Cell(154, 0, 'Subtotal Rp.   ' . $subtotal . ',-', 99, 1, 'L');
+        $pdf->Cell(10, 7, '', 0, 1);
+        $pdf->SetFont('Arial', '', 10);
+        $pdf->Cell(154, 0, 'Diskon   Rp.   ' . $diskon . ',-', 99, 1, 'L');
         $pdf->Cell(10, 7, '', 0, 1);
         $pdf->SetFont('Arial', 'B', 10);
-        $pdf->Cell(154, 0, 'TOTAL  Rp.   '.$total.',-', 99, 1, 'L');
-        $pdf->Output("I","[LUNAS] Struk - ".$kode.".pdf");
+        $pdf->Cell(154, 0, 'TOTAL  Rp.   ' . $total . ',-', 99, 1, 'L');
+        $pdf->Output("I", "[LUNAS] Struk - " . $kode . ".pdf");
     }
 
-
-
-    
-
-    function laporanJualMobil()
+    public function laporanJualMobil()
     {
         $cnt = 1;
         $pdf = new FPDF('l', 'mm', 'A4');
